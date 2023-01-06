2d1cc660112fb115f969a63947bb1aeef6850d86
Ilia Romanov <iliaromanov033@gmail.com>
2023-01-06 11:02:16 -0500

    lambda role

--- NLPpipelineApiCdkApp/lib/nl_ppipeline_api_cdk_app-stack.ts
+++ NLPpipelineApiCdkApp/lib/nl_ppipeline_api_cdk_app-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam'
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +7,35 @@ export class NlPpipelineApiCdkAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'NlPpipelineApiCdkAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // IAM role for lambda which grants access to logs and cloudwatch metrics
+    let lambdaRole = new iam.Role(this, "LambdaRole", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+      inlinePolicies: {
+        "lambda-executor": new iam.PolicyDocument({
+          assignSids: true,
+          statements: [
+            new iam.PolicyStatement({
+              effect: iam.Effect.ALLOW,
+              actions: [
+                "ec2:DescribeTags",
+                "cloudwatch:GetMetricStatistics",
+                "cloudwatch:ListMetrics",
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents",
+                "logs:DescribeLogStreams"
+              ],
+              resources: ["*"]
+            }),
+            new iam.PolicyStatement({
+              effect: iam.Effect.ALLOW,
+              actions: ["lambda:InvokeFunction"],
+              resources: ["*"]
+            })
+          ]
+        })
+      }
+    });
+    
   }
 }
