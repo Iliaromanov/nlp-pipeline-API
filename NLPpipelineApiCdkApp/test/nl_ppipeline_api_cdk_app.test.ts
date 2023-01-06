eb38ce4581cc9f03fd1bbe042cf52ef377653664
Ilia Romanov <iliaromanov033@gmail.com>
2023-01-05 19:59:46 -0500

    cdk init

---
+++ NLPpipelineApiCdkApp/test/nl_ppipeline_api_cdk_app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NlPpipelineApiCdkApp from '../lib/nl_ppipeline_api_cdk_app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/nl_ppipeline_api_cdk_app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NlPpipelineApiCdkApp.NlPpipelineApiCdkAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
