efed9238b9cfd34f0b0c40bc612f2fda5175218c
Ilia Romanov <iliaromanov033@gmail.com>
2023-01-06 11:22:57 -0500

    apigw lambda rest api interface definition

--- NLPpipelineApiCdkApp/lib/nlp_pipeline_api_cdk_app-stack.ts
+++ NLPpipelineApiCdkApp/lib/nlp_pipeline_api_cdk_app-stack.ts
@@ -2,8 +2,17 @@ import * as cdk from 'aws-cdk-lib';
 import { Duration, CfnOutput } from 'aws-cdk-lib';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+
+// For apigw throttling options to limit requests/second 
+//  and avoid unwated billing issues and apply
+//  token-buck style throttling to web app
+//  (https://en.wikipedia.org/wiki/Token_bucket)
+const MAX_RPS = 100; 
+const MAX_RPS_BUCKET_SIZE = 1000;
+
 
 export class NlPpipelineApiCdkAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -50,6 +59,18 @@ export class NlPpipelineApiCdkAppStack extends cdk.Stack {
       memorySize: 256,
       // logRetention: logs.RetentionDays.SIX_MONTHS, // default is infinite
     })
+
+    // Defining apigw lambda interface
+    let apigwRestApi = new apigw.LambdaRestApi(this, "NlpPipelineLambdaRestApi", {
+      restApiName: "nlp-pipeline-api",
+      handler: apiLambda,
+      deployOptions: {
+        throttlingBurstLimit: MAX_RPS_BUCKET_SIZE,
+        throttlingRateLimit: MAX_RPS
+      }
+    });
+    const restApiUrl = `${apigwRestApi.restApiId}.execute-api.${this.region}.amazonaws.com`;
+
     
   }
 }
