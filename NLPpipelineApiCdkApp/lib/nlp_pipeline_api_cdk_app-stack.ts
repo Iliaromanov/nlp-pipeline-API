68b8bc28917829243e8f6584f328c9b69f7dd579
Ilia Romanov <iliaromanov033@gmail.com>
2023-01-06 11:14:20 -0500

    lambda definition

--- NLPpipelineApiCdkApp/lib/nlp_pipeline_api_cdk_app-stack.ts
+++ NLPpipelineApiCdkApp/lib/nlp_pipeline_api_cdk_app-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
-import * as iam from 'aws-cdk-lib/aws-iam'
+import { Duration, CfnOutput } from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -36,6 +38,18 @@ export class NlPpipelineApiCdkAppStack extends cdk.Stack {
         })
       }
     });
+
+    // Defining lambda
+    let apiLambda = new lambda.Function(this, "NlpLambda", {
+      functionName: "nlp-pipeline-lambda",
+      code: lambda.Code.fromAsset(__dirname + "/../build-python"), // creating by make
+      runtime: lambda.Runtime.PYTHON_3_8,
+      handler: "serverless_fastapi.lambda.lambda_handler",
+      role: lambdaRole,
+      timeout: Duration.seconds(30),
+      memorySize: 256,
+      // logRetention: logs.RetentionDays.SIX_MONTHS, // default is infinite
+    })
     
   }
 }
